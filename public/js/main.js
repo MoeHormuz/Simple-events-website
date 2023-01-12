c0fa2012ace9f4032d2e5e9ce55e71af5166e3b9
Muhannad Hurmiz <MoeHurmiz@gmail.com>
2023-01-12 23:02:38 +0300

    just arranged the codes

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,17 @@
 function deleteEvent() {
     let btn = document.getElementById('deleteBtn')
     let id = btn.getAttribute('data-id')
-    
-    axios.delete('/events/delete/'+ id)
-    .then( (res)=> {
-        console.log(res.data)
-        window.location.href = '/events'
-    })
 
-    .catch( (err)=> {
+    axios.delete('/events/delete/' + id)
+        .then((res) => {
+            alert('Clicking "OK" will permanently delete the event')
+            console.log(res.data)
+            window.location.href = '/events'
+        })
 
-        console.log(err)
-    })
+        .catch((err) => {
+
+            console.log(err)
+        })
 
 }
