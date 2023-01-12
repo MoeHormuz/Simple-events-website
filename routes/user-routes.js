3e22aad59e09f4741c8f795d05d06901d54abeb9
Muhannad Hurmiz <MoeHurmiz@gmail.com>
2023-01-13 01:21:09 +0300

    Added an avatar to the profile

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,6 +2,18 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const passport = require('passport')
+const multer = require('multer')
+// configure multer:
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.png')
+    }
+})
+const upload = multer({ storage: storage })
+
 
 
 // middleware to check if user is logged in:
@@ -47,6 +59,20 @@ router.post('/login',
 // profile 
 router.get('/profile', isAuthenticated, (req, res) => {
     res.render('user/profile', { success: req.flash('success') })
+})
+
+
+// upload user avatar:
+router.post('/uploadAvatar', upload.single('avatar'), (req, res) => {
+    let newFields = {
+        avatar: req.file.filename
+    }
+
+    User.updateOne({ _id: req.user.id }, newFields, (err) => {
+        if (!err) {
+            res.redirect('/users/profile')
+        }
+    })
 })
 
 
