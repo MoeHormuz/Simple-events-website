1b2c9c0c30ce3c733ea4a4f8ebcc129fc713b588
Muhannad Hormuz <moehormuz.gmail.com>
2023-01-15 14:41:08 +0300

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
+// Delete Events:
 function deleteEvent() {
     let btn = document.getElementById('deleteBtn')
     let id = btn.getAttribute('data-id')
 
     axios.delete('/events/delete/' + id)
         .then((res) => {
-            alert('Clicking "OK" will permanently delete the event')
             console.log(res.data)
             window.location.href = '/events'
         })
@@ -14,4 +14,36 @@ function deleteEvent() {
             console.log(err)
         })
 
+}
+
+
+// Upload Avatar:
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader()
+
+        reader.onload = function (e) {
+
+            let uploadBTN = document.getElementById("uploadButton")
+            uploadBTN.style.display = "inline"
+
+            let cancelBTN = document.getElementById("cancelButton")
+            cancelBTN.style.display = "inline"
+
+            let title = document.getElementById("titleImagePlaceHolder")
+            title.style.display = "inline"
+
+            let image = document.getElementById("imagePlaceHolder")
+            image.style.display = "inline"
+            image.src = e.target.result
+        }
+
+        reader.readAsDataURL(input.files[0])
+    }
+}
+
+function cancelBTN() {
+    window.location.href = '/users/profile'
 }
